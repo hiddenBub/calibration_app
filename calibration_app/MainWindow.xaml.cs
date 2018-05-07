c413cf6bba1a0f08cd43189a297b09e7bb68accd
ww <wwkillleng@sina.com>
2018-05-07 17:50:28 +0800

    提交项目

--- calibration_app/MainWindow.xaml.cs
+++ calibration_app/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace calibration_app
 {
@@ -20,9 +22,122 @@ namespace calibration_app
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isGather = false;
+
+
+        
+
+        public bool IsGather
+        {
+            get => isGather;
+            set {
+                if ( value != false && value != true)
+                {
+                    isGather = false;
+                } 
+                else
+                {
+                    isGather = value;
+                }
+                
+            } 
+                
+           
+        }
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+       
+        private void ImportDataMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 菜单栏采集状态切换
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GatherMenu_Click(object sender, RoutedEventArgs e)
+        {
+            bool state = IsGather;
+            // 没有开始采集时开始采集，并将说明文字更改为结束采集
+            SwitchGather(state);
+        }
+
+        /// <summary>
+        /// 工具栏采集状态切换
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GatherCB_Click(object sender, RoutedEventArgs e)
+        {
+            bool state = IsGather;
+            // 没有开始采集时开始采集，并将说明文字更改为结束采集
+            SwitchGather(state);
+        }
+
+        private void CloseWindowMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SettingMenu_Click(object sender, RoutedEventArgs e)
+        {
+            SettingWindow window = new SettingWindow
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+            window.ShowDialog();
+        }
+
+        private void SettingBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SettingWindow window = new SettingWindow()
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+            window.ShowDialog();
+        }
+
+
+
+
+
+        /// <summary>
+        /// 切换采集开关
+        /// </summary>
+        /// <param name="state">当前是否在采集中</param>
+        public void SwitchGather(bool state)
+        {
+            if (!state)
+            {
+                GatherCB.Content = GatherMenu.Header = "结束采集";
+            }
+            else
+            {
+                GatherCB.Content = GatherMenu.Header = "开始采集";
+            }
+            IsGather = !state;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SeriesCollection SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double> { 1,2,3,4,5 }
+                },
+                new ColumnSeries
+                {
+                    Values = new ChartValues<decimal> { 5,4,3,2,1 }
+                }
+            };
         }
     }
 }
