9e905df0d247f1b421c42ed804f784cc315eae42
ww <wwkillleng@sina.com>
2018-05-29 19:01:34 +0800

    完善mainwindow

--- calibration_app/MainWindow.xaml.cs
+++ calibration_app/MainWindow.xaml.cs
@@ -169,18 +169,80 @@ namespace calibration_app
                     Filter = "Excel文件(*.xls)|*"
                 };
                 ofd.ShowDialog();
+                // 获取文件路径
                 string filePath = ofd.FileName;
-
-                string strConn = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + filePath + "; Extended Properties = 'Excel 12.0 Xml; HDR = No'";
-                DataSet ds = new DataSet();
-                OleDbDataAdapter oada = new OleDbDataAdapter("select * from [Sheet1$]", strConn);
-                oada.Fill(ds);
-                DataTable dt = ds.Tables[0];
-                List<String[]> list = new List<string[]>();
-                for (int i = 0; i < dt.Rows.Count; i++)
+                // 如果选择文件不为空
+                if (filePath != "" && filePath != null)
                 {
-                    Console.Write(dt.Rows[i]["DATE"] + " " + dt.Rows[i]["GHI"] + " " + dt.Rows[i]["GTI"] + "\n");
+                    // 配置连接字符串
+                    string strConn = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + filePath + "; Extended Properties = 'Excel 12.0 Xml; HDR = No'";
+                    // 创建新的数据集
+                    DataSet ds = new DataSet();
+                    // 获取数据
+                    OleDbDataAdapter oada = new OleDbDataAdapter("select * from [Sheet1$]", strConn);
+                    // 将数据填充至数据集中
+                    oada.Fill(ds);
+                    // 存储列表
+                    DataTable dt = ds.Tables[0];
+                    // 存储表头长度+1即与dat文件存储格式相同
+                    int length = dt.Columns.Count + 1;
+                    //"\"" + string.Join("\",\"", new string[] { Setting.Project.Name, Setting.Project.Lng.ToString(), Setting.Project.Lat.ToString() }) + "\"",
+                    //"\"" + string.Join("\",\"", new string[] { "TIMESTAMP", "RECORD", "GHI_80A", "GHI_80B", "GHI_80C" }) + "\"",
+                    //"\"" + string.Join("\",\"", new string[] { "TS", "RN", "W/m2", "W/m2", "W/m2" }) + "\"",
+                    //"\"" + string.Join("\",\"", new string[] { "", "", "Avg", "Avg", "Avg" }) + "\"",
+                    // 初始化dat文件头
+                    List<string[]> header = new List<string[]>
+                    {
+                        new string[] { Setting.Project.Name,Setting.Project.Lng.ToString(), Setting.Project.Lat.ToString()},
+                        new string[length],
+                        new string[length],
+                        new string[length],
+                    };
+                    // 填充字段之前的数据
+                    header[1][0] = "TIMESTAMP";header[1][1] = "RECORD";
+                    header[2][0] = "TS"; header[2][1] = "RN";
+                    header[3][0] = "";header[3][1] = "";
+                    // 填充字段
+                    for (int j = 1; j < dt.Columns.Count;j++)
+                    {
+                        header[1][j +1] = dt.Columns[j].ToString();
+                        header[2][j + 1] = "W/m2";
+                        header[3][j + 1] = "Avg";
+                    }
+                    // 创建以行为区分的数据列表
+                    List<string> headerNew = new List<string>();
+                    // 循环遍历原列表并将字符串数据存至新列表
+                    for (int k= 0;k<header.Count;k++)
+                    {
+                        headerNew.Add("\"" + string.Join("\",\"",header[k]) + "\"");
+                    }
+                    // 生成校准数据文件名
+                    string fileName = GetFileName(DataType.CalibrationData, (DateTime)GatherTimer[0], GatherTimer[1]);
+                    // 为该文件添加表头
+                    AddDatHeader(headerNew, fileName);
+                    
+                    List<String[]> list = new List<string[]>();
+                    // 生成文件写入实例，准备写入文件
+                    StreamWriter sw = new StreamWriter(new FileStream(fileName, FileMode.Append));
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+
+                        Console.Write(dt.Rows[i][dt.Columns[0]] + " " + dt.Rows[i][dt.Columns[1]] + " " + dt.Rows[i][dt.Columns[2]] + "\n");
+                        // 初始化数据(单行)字符串
+                        string str = "\""+DateFormat(Convert.ToDateTime(dt.Rows[i][dt.Columns[0]]),"yyyy-MM-dd HH:mm:ss") + "\","+ i;
+                        // 填充数据
+                        for (int ii = 1;ii< dt.Columns.Count; ii++)
+                        {
+                            str += "," + dt.Rows[i][dt.Columns[ii]];
+                        }
+                        // 添加行结束符
+                        //str += Environment.NewLine;
+                        sw.WriteLine(str);
+                    }
+                    sw.Close();
+                    MessageBox.Show("导入完成");
                 }
+                
             }
             else
             {
